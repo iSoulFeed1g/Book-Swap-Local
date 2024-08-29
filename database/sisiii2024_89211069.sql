bf1eac81aa223ff8473e3c69e3007cd48017eb89
Vanja Antonovic <vanjaantonovic@gmail.com>
2024-08-29 18:09:56 +0200

    Updated database file for deployment

--- database/sisiii2024_89211069.sql
+++ database/sisiii2024_89211069.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 4.9.5deb2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:5306
--- Generation Time: Jun 28, 2024 at 11:50 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: localhost:3306
+-- Generation Time: Aug 29, 2024 at 05:57 PM
+-- Server version: 10.3.39-MariaDB-0ubuntu0.20.04.2
+-- PHP Version: 7.4.3-4ubuntu2.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `sisiii2024_89211069`
+-- Database: `SISIII2024_89211069`
 --
 
 -- --------------------------------------------------------
@@ -30,18 +31,52 @@ SET time_zone = "+00:00";
 CREATE TABLE `chats` (
   `id` int(11) NOT NULL,
   `buyer_id` int(11) DEFAULT NULL,
-  `seller_id` int(11) DEFAULT NULL
+  `seller_id` int(11) DEFAULT NULL,
+  `post_id` int(11) DEFAULT NULL,
+  `postImage` varchar(255) DEFAULT NULL,
+  `postTitle` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `chats`
 --
 
-INSERT INTO `chats` (`id`, `buyer_id`, `seller_id`) VALUES
-(20, 1, 2),
-(21, 3, 2),
-(22, 3, 1),
-(24, 4, 1);
+INSERT INTO `chats` (`id`, `buyer_id`, `seller_id`, `post_id`, `postImage`, `postTitle`) VALUES
+(29, 1, 5, 7, NULL, NULL),
+(30, 1, 2, 5, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genres`
+--
+
+CREATE TABLE `genres` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `genres`
+--
+
+INSERT INTO `genres` (`id`, `name`) VALUES
+(11, 'Adventure'),
+(15, 'Children’s'),
+(12, 'Drama'),
+(4, 'Fantasy'),
+(1, 'Fiction'),
+(10, 'Historical Fiction'),
+(8, 'Horror'),
+(5, 'Mystery'),
+(2, 'Non-fiction'),
+(7, 'Poetry'),
+(6, 'Romance'),
+(3, 'Science Fiction'),
+(13, 'Self-help'),
+(9, 'Thriller'),
+(16, 'Tragedy'),
+(14, 'Travel');
 
 -- --------------------------------------------------------
 
@@ -62,10 +97,11 @@ CREATE TABLE `login` (
 --
 
 INSERT INTO `login` (`id`, `name`, `email`, `password`, `profile_pic`) VALUES
-(1, 'Vanja', 'vanjaantonovic@gmail.com', 'Vanja12345', 'uploads\\1719497065815.jpg'),
+(1, 'Vanja', 'vanjaantonovic@gmail.com', 'VanjaDoca123', 'uploads\\1721840466734.jpg'),
 (2, 'Andriana', 'andriannajjj@gmail.com', 'Andriana12345', 'uploads/default_image.png'),
 (3, 'Stefan', 'stefanmzt@gmail.com', 'Stefan123', 'uploads\\1719527096282.jpg'),
-(4, 'Filip', 'filipantonovic@gmail.com', 'Filip12345', 'uploads/default_image.png');
+(4, 'Filip', 'filipantonovic@gmail.com', 'Filip12345', 'uploads/default_image.png'),
+(5, 'Andrija', 'andrijapirot16@gmail.com', 'Andrija12345', 'uploads/default_image.png');
 
 -- --------------------------------------------------------
 
@@ -78,31 +114,21 @@ CREATE TABLE `messages` (
   `chat_id` int(11) DEFAULT NULL,
   `user_email` varchar(255) DEFAULT NULL,
   `message` text DEFAULT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  `postImage` varchar(255) DEFAULT NULL,
+  `postTitle` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `messages`
 --
 
-INSERT INTO `messages` (`id`, `chat_id`, `user_email`, `message`, `timestamp`) VALUES
-(9, 21, 'stefanmzt@gmail.com', 'Hello', '2024-06-28 18:40:05'),
-(10, 22, 'stefanmzt@gmail.com', 'Good Book', '2024-06-28 18:40:22'),
-(11, 20, 'vanjaantonovic@gmail.com', 'Nice book by my man steven', '2024-06-28 18:41:10'),
-(12, 20, 'vanjaantonovic@gmail.com', 'Dreamcathcer', '2024-06-28 18:43:54'),
-(13, 20, 'andriannajjj@gmail.com', 'Hahah yea', '2024-06-28 18:44:23'),
-(14, 21, 'andriannajjj@gmail.com', 'Hello stefan from andriana', '2024-06-28 18:44:42'),
-(15, 20, 'vanjaantonovic@gmail.com', 'Niceee', '2024-06-28 19:35:34'),
-(16, 22, 'vanjaantonovic@gmail.com', 'Yea it is', '2024-06-28 19:42:12'),
-(17, 20, 'andriannajjj@gmail.com', 'Do you want to buy it?', '2024-06-28 19:43:05'),
-(18, 21, 'andriannajjj@gmail.com', 'Do you want to buy the book or what?', '2024-06-28 19:43:18'),
-(19, 22, 'stefanmzt@gmail.com', 'I want to buy it', '2024-06-28 19:43:47'),
-(20, 22, 'vanjaantonovic@gmail.com', 'Can you do like 100$ for it?', '2024-06-28 20:13:36'),
-(21, 20, 'andriannajjj@gmail.com', 'Are you there??', '2024-06-28 21:20:38'),
-(22, 20, 'andriannajjj@gmail.com', 'This is a good book also. I want to buy it', '2024-06-28 21:30:37'),
-(23, 20, 'vanjaantonovic@gmail.com', 'Hm I might want to sell it to you', '2024-06-28 21:31:55'),
-(24, 24, 'filipantonovic@gmail.com', 'Hey I might be interested in buying the Divine Comedy off of you :D', '2024-06-28 21:49:27'),
-(25, 24, 'vanjaantonovic@gmail.com', 'Don\'t send me those cringe smilies', '2024-06-28 21:49:50');
+INSERT INTO `messages` (`id`, `chat_id`, `user_email`, `message`, `timestamp`, `postImage`, `postTitle`) VALUES
+(42, 30, 'vanjaantonovic@gmail.com', 'Dreaming', '2024-07-24 11:36:45', NULL, NULL),
+(43, 30, 'vanjaantonovic@gmail.com', 'Picuture?', '2024-07-24 11:40:53', NULL, NULL),
+(46, 29, 'vanjaantonovic@gmail.com', 'OKAY', '2024-07-24 12:03:41', NULL, NULL),
+(48, 29, NULL, 'PLASE SHOW THE PIC', '2024-07-24 12:11:16', 'uploads\\1719935463177.jpg', 'To Kill a Mockingbird'),
+(49, 29, 'vanjaantonovic@gmail.com', 'IS this right?', '2024-07-24 12:13:55', 'uploads\\1719935463177.jpg', 'To Kill a Mockingbird');
 
 -- --------------------------------------------------------
 
@@ -118,18 +144,22 @@ CREATE TABLE `posts` (
   `price` decimal(10,2) DEFAULT NULL,
   `picture` varchar(255) DEFAULT NULL,
   `time` timestamp NOT NULL DEFAULT current_timestamp(),
-  `author` varchar(255) DEFAULT NULL
+  `author` varchar(255) DEFAULT NULL,
+  `genre` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `posts`
 --
 
-INSERT INTO `posts` (`id`, `title`, `description`, `email`, `price`, `picture`, `time`, `author`) VALUES
-(1, 'Shoe At The End of The World', 'I am selling the book \"Shoe At The End of The World\" it\'s in a very good condition.', 'vanjaantonovic@gmail.com', 15.00, 'uploads\\1719487720875.jpg', '2024-06-27 11:28:40', 'Dejan Aleksić'),
-(3, 'Percy Jackson & the Olympians: The Lightning Thief', 'Percy Jackson & the Olympians: The Lightning Thief book', 'vanjaantonovic@gmail.com', 15.00, 'uploads\\1719494445380.jpg', '2024-06-27 13:20:45', 'Rick Riordan'),
-(4, 'THE DIVINE COMEDY', 'I am selling the book \"Divine Comedy\" by Dante. Good condition', 'vanjaantonovic@gmail.com', 30.00, 'uploads\\1719526922131.jpg', '2024-06-27 13:47:40', 'Dante Alighieri'),
-(5, 'Dreamcatcher', 'I am selling the book \"Dreamcatcher\" a novel by Stephen King. It\'s in excellent condition', 'andriannajjj@gmail.com', 20.00, 'uploads\\1719496906972.jpg', '2024-06-27 14:01:46', 'Stephen King');
+INSERT INTO `posts` (`id`, `title`, `description`, `email`, `price`, `picture`, `time`, `author`, `genre`) VALUES
+(1, 'Shoe At The End of The World', 'I am selling the book \"Shoe At The End of The World\" it\'s in a very good condition.', 'vanjaantonovic@gmail.com', '15.00', 'uploads\\1719487720875.jpg', '2024-06-27 11:28:40', 'Dejan Aleksić', 'Children’s'),
+(3, 'Percy Jackson & the Olympians: The Lightning Thief', 'Percy Jackson & the Olympians: The Lightning Thief book', 'vanjaantonovic@gmail.com', '15.00', 'uploads\\1719494445380.jpg', '2024-06-27 13:20:45', 'Rick Riordan', 'Fiction'),
+(4, 'THE DIVINE COMEDY', 'I am selling the book \"Divine Comedy\" by Dante. Good condition', 'vanjaantonovic@gmail.com', '30.00', 'uploads\\1719526922131.jpg', '2024-06-27 13:47:40', 'Dante Alighieri', 'Poetry'),
+(5, 'Dreamcatcher', 'I am selling the book \"Dreamcatcher\" a novel by Stephen King. It\'s in excellent condition', 'andriannajjj@gmail.com', '20.00', 'uploads\\1719496906972.jpg', '2024-06-27 14:01:46', 'Stephen King', 'Horror'),
+(7, 'To Kill a Mockingbird', 'Hello guys. I am selling the \"To Kill a Mockingbird\" book. I finished it and I want to get rid of it. ', 'andrijapirot16@gmail.com', '7.00', 'uploads\\1719935463177.jpg', '2024-07-02 15:51:03', 'Harper Lee', 'Thriller'),
+(9, 'The Lord of the Rings', 'I am selling a Lord of the Rings book. PERFECT condition ', 'vanjaantonovic@gmail.com', '15.00', 'uploads\\1721849205754.jpg', '2024-07-24 19:26:45', 'J.R.R. Tolkien', 'Fiction'),
+(10, 'The Great Gatsby', 'I am selling the book \"The Great Gatsby\" It\'s still unopened so that is why the price is so high, it\'s a collector\'s piece. For a serious buyer, the price can be negotiated down.', 'filipantonovic@gmail.com', '250.00', 'uploads\\1721948223159.jpg', '2024-07-25 22:57:03', 'F. Scott Fitzgerald', 'Tragedy');
 
 --
 -- Indexes for dumped tables
@@ -144,6 +174,13 @@ ALTER TABLE `chats`
   ADD KEY `chats_ibfk_2` (`seller_id`);
 
 --
+-- Indexes for table `genres`
+--
+ALTER TABLE `genres`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_name` (`name`);
+
+--
 -- Indexes for table `login`
 --
 ALTER TABLE `login`
@@ -154,13 +191,16 @@ ALTER TABLE `login`
 -- Indexes for table `messages`
 --
 ALTER TABLE `messages`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_chats_messages` (`chat_id`);
 
 --
 -- Indexes for table `posts`
 --
 ALTER TABLE `posts`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_email_posts` (`email`),
+  ADD KEY `fk_genre_posts` (`genre`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -170,25 +210,31 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT for table `chats`
 --
 ALTER TABLE `chats`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT for table `genres`
+--
+ALTER TABLE `genres`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Constraints for dumped tables
@@ -199,7 +245,24 @@ ALTER TABLE `posts`
 --
 ALTER TABLE `chats`
   ADD CONSTRAINT `chats_ibfk_1` FOREIGN KEY (`buyer_id`) REFERENCES `login` (`id`),
-  ADD CONSTRAINT `chats_ibfk_2` FOREIGN KEY (`seller_id`) REFERENCES `login` (`id`);
+  ADD CONSTRAINT `chats_ibfk_2` FOREIGN KEY (`seller_id`) REFERENCES `login` (`id`),
+  ADD CONSTRAINT `fk_buyer_chats` FOREIGN KEY (`buyer_id`) REFERENCES `login` (`id`),
+  ADD CONSTRAINT `fk_login_buyer` FOREIGN KEY (`buyer_id`) REFERENCES `login` (`id`),
+  ADD CONSTRAINT `fk_login_seller` FOREIGN KEY (`seller_id`) REFERENCES `login` (`id`),
+  ADD CONSTRAINT `fk_seller_chats` FOREIGN KEY (`seller_id`) REFERENCES `login` (`id`);
+
+--
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `fk_chats_messages` FOREIGN KEY (`chat_id`) REFERENCES `chats` (`id`);
+
+--
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `fk_email_posts` FOREIGN KEY (`email`) REFERENCES `login` (`email`),
+  ADD CONSTRAINT `fk_genre_posts` FOREIGN KEY (`genre`) REFERENCES `genres` (`name`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
